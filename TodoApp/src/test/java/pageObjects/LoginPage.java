6d781bcb0df7bc78b435ca7ac1e69f0ee5a7b349
gopikrishnannm <gopikrishnannm01@gmail.com>
2025-01-21 22:21:11 +0530

    added first page object class for login

---
+++ TodoApp/src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,36 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+	
+	public LoginPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	@FindBy(id="username")
+	WebElement txtUsername;
+	
+	@FindBy(id="password")
+	WebElement txtPassword;
+	
+	@FindBy(id="register-button")
+	WebElement btnRegister;
+	
+	@FindBy(id="login-button")
+	WebElement btnLogin;
+	
+	public void setUsername(String username){
+		txtUsername.sendKeys(username);	
+	}
+	
+	public void setPassword(String password) {
+		txtPassword.sendKeys(password);
+	}
+	
+	public void clickLogin() {
+		btnLogin.click();
+	}
+}
---
+++ TodoApp/src/test/java/testCases/BaseClass.java
@@ -0,0 +1,12 @@
+package testCases;
+
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass {
+	
+	@BeforeClass
+	public void setup() {
+		
+	}
+
+}
